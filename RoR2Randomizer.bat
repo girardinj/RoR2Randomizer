1acd9abfe7de3cce0c6fa489077f0620fd3df3ab
Jarod Girardin <jarod.girardin@he-arc.ch>
2022-04-04 23:54:02 +0200

    correct typo

--- RoR2Randomizer.bat
+++ RoR2Randomizer.bat
@@ -1,6 +1,6 @@
 @echo off
-echo "Activating python virtual environment..."
+echo Activating python virtual environment...
 call .venv/Scripts/activate.bat
-echo "Activated!"
-echo "Starting the application..."
+echo Activated!
+echo Starting the application...
 start /min pyw src/RoR2Randomizer.py

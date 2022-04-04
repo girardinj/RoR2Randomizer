dd68d1e9ec7d7a3e5a90920bec3557552c0204ad
Jarod Girardin <jarod.girardin@he-arc.ch>
2022-04-04 23:52:03 +0200

    add instructions in bat

--- RoR2Randomizer.bat
+++ RoR2Randomizer.bat
@@ -1,2 +1,6 @@
+@echo off
+echo "Activating python virtual environment..."
 call .venv/Scripts/activate.bat
+echo "Activated!"
+echo "Starting the application..."
 start /min pyw src/RoR2Randomizer.py
--- install.bat
+++ install.bat
@@ -1,3 +1,13 @@
+@echo off
+echo "Installing..."
 py -m venv .venv
+echo "Installed!"
+echo "Activating python virtual environment..."
 call .venv/Scripts/activate.bat
+echo "Activated!"
+echo "Installing dependencies..."
 pip install -r requirements.txt
+echo "Installed!"
+echo "You can now run the application"
+
+pause
